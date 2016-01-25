e8fe828cb4890325f22af3e6db1fdb390469fe1e
guruwanabe <bp.stanciu@gmail.com>
2016-01-25 18:24:26 +0200

    remove responsive

--- js/app.js
+++ js/app.js
@@ -67,30 +67,7 @@
 							vertical: blnVertical || false,
 							verticalSwiping: blnVertical || false,
 							centerMode: blnCentered || false,
-							centerPadding: '80px',
-							responsive: [
-								{
-									breakpoint: 1024,
-									settings: {
-										slidesToShow: 3,
-										slidesToScroll: 3
-									}
-								},
-								{
-									breakpoint: 600,
-									settings: {
-										slidesToShow: 2,
-										slidesToScroll: 2
-									}
-								},
-								{
-									breakpoint: 480,
-									settings: {
-										slidesToShow: 1,
-										slidesToScroll: 1
-									}
-								}
-							]
+							centerPadding: '80px'
 						};
 				//console.log(defaults);
 				var settings = $.extend(defaults, options);
